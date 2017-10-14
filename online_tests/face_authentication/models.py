dc414b79632b9df3f31a747f117bb50aa6651349
anssar <rumyancevandr@yandex.ru>
2017-10-14 06:28:09 +0000

    user model

--- online_tests/face_authentication/models.py
+++ online_tests/face_authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class FAUser(models.Model):
+    username = models.CharField(max_length=128)
+    token = models.CharField(max_length=128, blank=True, null=True)
+    photo= models.ImageField(upload_to='users')
+
+    def __str__(self):
+        return '{}'.format(self.username)
